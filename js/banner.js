20b0361517347428b9a1925c99fc76fa4ed98545
alissonalef121 <alissonalef121@gmail.com>
2022-03-27 18:29:58 -0300

    Banner added

---
+++ js/banner.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.banner_wrapper').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        dots: true
+    })
+})
